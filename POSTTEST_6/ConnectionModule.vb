d38e39e0c67841e8655c66a8ed77e81b277b603d
Marshaufaa <152855816+Marshaufaa@users.noreply.github.com>
2026-04-21 21:38:59 +0800

    Delete POSTTEST_6 directory

--- POSTTEST_6/ConnectionModule.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports MySqlConnector
-
-Public Module ConnectionModule
-    Private Const ConnString As String = "Server=localhost;Database=db_videotron;User ID=root;Password=;"
-
-    Public Function GetConnection() As MySqlConnection
-        Return New MySqlConnection(ConnString)
-    End Function
-End Module
--- POSTTEST_6/Penyewaan.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Penyewaan
-    Public Property ID As Integer
-    Public Property NamaPenyewa As String
-    Public Property LokasiVideotron As String
-    Public Property DurasiSewa As Integer
-    Public Property Harga As Decimal
-    Public Property TanggalSewa As DateTime
-End Class
